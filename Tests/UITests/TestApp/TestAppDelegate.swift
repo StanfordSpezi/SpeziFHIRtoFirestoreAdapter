cdc41c20028e6b1a4f37080c5b15fd2ac88d1a54
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-04-04 15:12:23 -0700

    CardinalKitFHIRToFirestoreAdapter

---
+++ Tests/UITests/TestApp/TestAppDelegate.swift
@@ -0,0 +1,24 @@
+//
+// This source file is part of the CardinalKit open-source project
+//
+// SPDX-FileCopyrightText: 2022 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+import CardinalKit
+import CardinalKitFHIRToFirestoreAdapter
+@preconcurrency import CardinalKitFHIR
+import CardinalKitFirestore
+import SwiftUI
+
+
+class TestAppDelegate: CardinalKitAppDelegate {
+    override var configuration: Configuration {
+        Configuration(standard: FHIR()) {
+            Firestore {
+                FHIRToFirestoreAdapter()
+            }
+        }
+    }
+}
